ea343e43f402463c6a78815d40e494daf491554d
anson416 <lamyiufung2003@gmail.com>
2024-05-17 16:21:47 +0800

    Add random agent probability

--- play.py
+++ play.py
@@ -15,6 +15,7 @@ def play(
     pkl_path: Optional[str] = None,
     agent_1: bool = False,
     agent_2: bool = False,
+    random_prob: float = 0.0,
     seed: Optional[int] = None,
 ) -> None:
     def get_user_action(player: Symbol) -> tuple[int, int]:
@@ -32,6 +33,8 @@ def play(
     # Check arguments
     if pkl_path is None and (agent_1 or agent_2):
         raise ValueError(f"`pkl_path` cannot be None when either `agent_1` or `agent_2` is True.")
+    if not (0 <= random_prob <= 1):
+        raise ValueError(f"`random_prob` is expected to be in range [0, 1], got {random_prob}.")
 
     # Seed random number generator
     random.seed(seed)
@@ -47,13 +50,15 @@ def play(
     player = Symbol.P1
     step = 0
     while True:
-        if step == 0 and agent_1:
-            action = random.choice(game.action_space)
-            game(*action, player)
-        elif (step % 2 == 0 and agent_1) or (step % 2 == 1 and agent_2):
-            for action, _ in agent(game.state):
-                if game(*action, player):
-                    break
+        if (step % 2 == 0 and agent_1) or (step % 2 == 1 and agent_2):
+            if (step == 0 and agent_1) or random.random() <= random_prob:
+                action = random.choice(game.action_space)
+                while not game(*action, player):
+                    action = random.choice(game.action_space)
+            else:
+                for action, _ in agent(game.state):
+                    if game(*action, player):
+                        break
         else:
             action = get_user_action(player)
             while not game(*action, player):
@@ -80,6 +85,14 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--pkl_path", type=str, default=None)
     parser.add_argument("-a1", "--agent_1", action="store_true")
     parser.add_argument("-a2", "--agent_2", action="store_true")
+    parser.add_argument("-r", "--random_prob", type=float, default=0.0)
     parser.add_argument("--seed", type=int, default=None)
     args = parser.parse_args()
-    play(size=args.size, pkl_path=args.pkl_path, agent_1=args.agent_1, agent_2=args.agent_2, seed=args.seed)
+    play(
+        size=args.size,
+        pkl_path=args.pkl_path,
+        agent_1=args.agent_1,
+        agent_2=args.agent_2,
+        random_prob=args.random_prob,
+        seed=args.seed,
+    )
